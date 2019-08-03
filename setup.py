c74d70b468945146be55b39fa529f2a89f37aeef
lemoinemeddy <53619610+lemoinemeddy@users.noreply.github.com>
2019-08-03 14:38:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Lemoine Meddy 2019 - Libre de fork - Free fork - Livre de fork
+# Auto install Flutter Debian system
+import os
+
+def execute(folder):
+    print("Get command line tools")
+    os.system("sudo apt install bash  curl  git  unzip   xz-utils ")
+    print("start download  Flutter SDK")
+    os.system("cd")
+    os.system("git clone https://github.com/flutter/flutter.git")
+    os.system("""echo export PATH="$PATH:$HOME/flutter/bin" > bash_profile""")
+    os.system("source bash_profile")
+    os.system("mkdir " + folder)
+    os.system("cd " + folder)
+    print("done !")
+    os.system("flutter doctor")
+    print("PLEASE INSTALL ANDROID SDK MANUALY IF NEEDED !")
+execute("labs")
